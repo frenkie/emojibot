83dbaf0c72b9fefdd695c29671ea731dfff9580c
frenkie <frank@frankbosma.nl>
2017-06-09 11:46:06 +0200

    sound and color all the things!

--- emoji/emoji.js
+++ emoji/emoji.js
@@ -4,14 +4,29 @@ $(function () {
     var $emojiContainer = $('.emoji-container');
     var emojiCssPrefix = 'emoji-container emoji-container--';
 
-    socket.on( 'neutral', function () {
-        $emojiContainer[ 0 ].className = emojiCssPrefix +'neutral';
-    } );
+    var allSounds = new Sounds( [
+        'audio/attention.mp3',
+        'audio/surprised.mp3',
+        'audio/sad.mp3',
+        'audio/angry.mp3',
+        'audio/hope.mp3',
+        'audio/excitement.mp3',
+        'audio/happy.mp3'
+    ] );
 
-    socket.on( 'angry', function () {
-        $emojiContainer[ 0 ].className = emojiCssPrefix +'angry';
-    } );
 
+    ['neutral', 'attention', 'surprised',
+    'sad','angry', 'hope', 'excitement', 'happy', 'shy', 'lookleft', 'lookright'
+    ].forEach( function ( emotion ) {
 
-    console.log('whooptiedoo');
+        socket.on( emotion, function () {
+            $emojiContainer[ 0 ].className = emojiCssPrefix + emotion;
+
+            if ( allSounds.has( 'audio/'+ emotion +'.mp3' ) ) {
+                allSounds.play( 'audio/'+ emotion +'.mp3' );
+            }
+        } );
+
+    });
+
 });
